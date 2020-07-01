78488b7c56f9cf4c848ae3974964c60ef41e1c9c
Rubén Zagon <ruben.zagon@gmail.com>
2020-07-01 17:39:02 +0100

    [WIP]

--- src/test/java/WorldShould.java
+++ src/test/java/WorldShould.java
@@ -1,21 +1,41 @@
-import game.World;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WorldShould {
 
+    // [0,0,0],[0,1,0],[0,0,1]],
 
     @Test
     public void it_works (){
-//        List<Integer[][]> listWorld = {};
-        List<Integer> listWorld = {};
+        List<Integer> sky = new ArrayList<Integer>();
+        List<Integer> ground = new ArrayList<Integer>();
+        List<Integer> water = new ArrayList<Integer>();
 
-        World world = new World(listWorld);
+        List<List<Integer>> world = new ArrayList<>();
 
-        assertThat().isEqualTo(1);
+        sky.add(0,0);
+        sky.add(1,0);
+        sky.add(2,0);
+
+        ground.add(0,0);
+        ground.add(1,0);
+        ground.add(2,0);
+
+        water.add(0,0);
+        water.add(1,0);
+        water.add(2,0);
+
+        world.add(0,sky);
+        world.add(1,ground);
+        world.add(2,water);
+
+        System.out.println(world);
+
+//        assertThat().isEqualTo(1);
     }
 
 }
